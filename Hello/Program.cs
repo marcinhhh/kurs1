92ffc972cfd2f6466d01b2f5fe668f7f2760b4bb
Marcin <marcin.harba@gmail.com>
2018-10-10 23:12:13 +0200

    Comitt all

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Hello
 {
@@ -10,6 +6,17 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            //WPisując na konsoli Hello Marcin przekazuje Marcin do
+            //tablicy argumentów args[0] i pojawia się Witaj Marcin
+            //Ograniczenie do pierwszego imienia
+            //string name = args[0];
+            //Console.WriteLine("Witaj, " + name);
+       
+            //To samo tylko więcej imion mogę wprowadzić
+            foreach (var item in args)
+            {
+                Console.WriteLine("Hello, " + item);    
+            }
         }
     }
 }
